e2ec1839a6ec1fe63ad9bdc81011fc2c2838dfce
Lucas <lucasbrito_9@hotmail.com>
2020-05-24 20:16:45 -0300

    Calculate the BMI and added .gitignore.

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -9,12 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func updateHeight(_ sender: UISlider) {
+        heightLabel.text = String(format: "%.2f", sender.value) + " m"
+    }
+    
+    @IBAction func updateWeigth(_ sender: UISlider) {
+        weightLabel.text = String(format: "%.2f", sender.value) + " Kg"
+    }
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        let height = heightSlider.value
+        let weight = weightSlider.value
+        
+        let bmi = weight / (height * height)
+        
+        print(bmi)
+    }
+    
 }
 
